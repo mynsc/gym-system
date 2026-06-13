ef17174f0d4bb3fa1f71d59680530521cc528d2d
mynsc <mcerquin.10@gmail.com>
2026-06-13 15:50:38 -0500

    ordenar clase Atleta

--- src/com/unmsm/gym/models/Atleta.java
+++ src/com/unmsm/gym/models/Atleta.java
@@ -1,40 +1,22 @@
 package src.com.unmsm.gym.models;
 
 public class Atleta extends Persona {
-    private String sport;
-    private String level;
-    private int trainingHours;
+    private String deporte;
 
-    public Atleta(int id, String username, String password, String name, String studentCode, String sport, String level, int trainingHours) {
-        super(id, username, password, name, studentCode);
+    public Atleta(
+        int id, 
+        String nombre, 
+        String apellido, 
+        String nombreDeUsuario, 
+        String contrasenia, 
+        String deporte) {
 
-        this.sport = sport;
-        this.level = level;
-        this.trainingHours = trainingHours;
+        super(id, nombre, apellido, nombreDeUsuario, contrasenia);
+        this.deporte = deporte;
     }
 
-    public void train() {
-        trainingHours++;
+    public void mostrarInformacionPersonal() {
+        super.mostrarInformacionPersonal();
+        System.out.println("Deporte: " + deporte);
     }
-
-    public void complete() {
-        if (trainingHours > 0) {
-            hasGamification.AwardPoints(trainingHours * 5);
-            trainingHours = 0;
-        }
-    }
-
-    @Override
-    public boolean canAccess() {
-        return !hasActivePenalty;
-    }
-
-    public String getSport() { return sport; }
-    public void setSport(String sport) { this.sport = sport; }
-
-    public String getLevel() { return level; }
-    public void setLevel(String level) { this.level = level; }
-
-    public int getTrainingHours() { return trainingHours; }
-    public void setTrainingHours(int trainingHours) { this.trainingHours = trainingHours; }
 }
