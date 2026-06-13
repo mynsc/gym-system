0e6c69b6ec9b1cd4d83491a88ec7422a3f63ea4a
mynsc <mcerquin.10@gmail.com>
2026-06-13 15:47:13 -0500

    simplificar clase Administrador

--- src/com/unmsm/gym/models/Administrador.java
+++ src/com/unmsm/gym/models/Administrador.java
@@ -4,19 +4,4 @@ public class Administrador extends Persona {
     public Administrador(int id, String username, String password, String name, String studentCode) {
         super(id, username, password, name, studentCode);
     }
-
-    public void overridePenalty(Persona user) {
-        if (user != null) {
-            user.clearPenalties();
-        }
-    }
-
-    public int manageCapacity(int newCapacity) {
-        return newCapacity > 0 ? newCapacity : 0;
-    }
-
-    @Override
-    public boolean canAccess() {
-        return true;
-    }
 }
